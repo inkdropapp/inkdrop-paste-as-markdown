15b3f4441de930485be0ce3a8c814f928962e8f5
Takuya Matsuyama <hi@craftz.dog>
2021-11-27 12:00:08 +0900

    fix(command): rename command

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { pasteHTML } from './paste-html'
 module.exports = {
   activate() {
     this.subscription = inkdrop.commands.add(document.body, {
-      'paste-as-markdown': () => pasteHTML()
+      'paste-as-markdown:paste': () => pasteHTML()
     })
   },
 
